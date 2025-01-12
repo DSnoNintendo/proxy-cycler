069a380ae914d67022e0edbe6ecb14b8e9a5f6f0
Daryl Stronge <darylkstronge@gmail.com>
2025-01-11 20:27:57 -0800

    add proxy dictionary key configuration

--- ProxyCycler/__init__.py
+++ ProxyCycler/__init__.py
@@ -1,11 +1,18 @@
+from typing import Optional, List
+
+
 class ProxyCycler:
-    def __init__(self, proxies: List[Optional] = None):
+    def __init__(self, proxies: List[Optional] = None, keys: List[Optional] = None):
         self.proxies = set(proxies) if proxies else set()
         self.current_proxy = proxies[0]
+        if not keys:
+            self.keys = ["http", "https"]
+        else:
+            self.keys = keys
 
     def cycle_proxies(self):
         if len(self.proxies) == 0:
-            raise AttributeError("No proxies supplied. Cannot cycle.")
+            raise ValueError("No proxies supplied. Cannot cycle.")
         if len(self.proxies) == 1:
             self.current_proxy = self.proxies.pop()
             self.proxies.add(self.current_proxy)
@@ -21,14 +28,10 @@ class ProxyCycler:
                 if proxy not in self.proxy_cycle:
                     self.current_proxy = proxy
 
+        return self.create_proxy_dict()
+
     def add_proxy(self, proxy: str):
         self.proxies.add(proxy)
 
-    def create_proxy_dict(self, keys: Optional[List] = None):
-        if keys is None:
-            return {
-                "http": self.current_proxy,
-                "https": self.current_proxy,
-            }
-        else:
-            return {key: self.current_proxy for key in keys}
+    def create_proxy_dict(self):
+        return {key: self.current_proxy for key in self.keys}
